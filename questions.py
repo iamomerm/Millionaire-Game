144f0f489e149c792acf5277f81247acb0543c57
Omer Menashe <46810647+iamomerm@users.noreply.github.com>
2023-11-11 18:48:13 +0200

    Update questions.py

--- questions.py
+++ questions.py
@@ -7,7 +7,7 @@ Questions = [
   {"question": "How many KM in a Mile", "answers": ["1", "2.4", "1.6", "0.7"], "correct_answer": 3},
   {"question": "What is the previous name of New-York", "answers": ["New-Amsterdam", "York", "None", "New-Zealand"], "correct_answer": 1},
   {"question": "Which beer is made in Netherlands", "answers": ["Heineken", "Guinness", "Budweiser", "Goldstar"], "correct_answer": 1},
-  {"question": "What is the square root of 2.25", "answers": ["1.25", "4", "0.5", "1.5"], "correct_answer": 4, 'num_for_friend': 0, 'num_for_quit': 6},
+  {"question": "What is the square root of 2.25", "answers": ["1.25", "4", "0.5", "1.5"], "correct_answer": 4},
   {"question": "Your grandmothers son is your", "answers": ["Dad", "Grandfather", "Aunt", "Brother"], "correct_answer": 1},
   {"question": "Santa Fe is a city of which country", "answers": ["Italy", "Ukraine", "Mexico", "Spain"], "correct_answer": 3},
   {"question": "Who painted the Mona Lisa", "answers": ["Picasso", "Van Gogh", "Da Vinci", "Salvador Dali"], "correct_answer": 3},
